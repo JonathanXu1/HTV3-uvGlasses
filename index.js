d151ecb0d4ac79732be57dc0ea4884161d6bf59a
Jonathan <jonathanxu25@gmail.com>
2019-02-24 03:03:55 -0500

    added socket.io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var io = require('socket.io')(http);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +9,20 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+io.on('connection', function(socket) {
+  console.log('User connected: ' + socket.id);
+  //socket.emit('light', light);
+  socket.on('disconnect', function(){
+    console.log('User disconnected: ' + socket.id);
+  });
+  socket.on('updateData', function(object){
+    data = object;
+    console.log("Update: ");
+    console.log(data);
+    io.sockets.emit('updateData', data);
+  });
+  socket.on("getData", function(){
+    io.sockets.emit('updateData', data);
+  });
+});
--- sketch_feb24a/sketch_feb24a.ino
+++ sketch_feb24a/sketch_feb24a.ino
@@ -4,10 +4,20 @@
 
 #include <Wire.h>
 #include <SSD1306.h>
+#include <SocketIOClient.h>
+#include <ArduinoJson.h>
 
 // Initialize the OLED display using Wire library
 SSD1306  display(0x3c, D3, D5);
 
+// Networking variables
+const char* ssid = "nO wiReLEsS hOTsPoTs";
+const char* password = "ZuccMe123";
+String host = "superjunction.herokuapp.com";
+int port = 5000;
+DynamicJsonBuffer jsonBuffer(JSON_OBJECT_SIZE(3));
+String JSON;
+SocketIOClient socket;
 
 void setup() {
   // put your setup code here, to run once:
